cd30c63dcd4d9e1a935a1c90b37d5261bf4fe583
devel0624 <devel062@gmail.com>
2024-07-07 21:29:23 +0900

    Initial commit

---
+++ src/main/java/com/galgoya/user/GalgoyaUserApplication.java
@@ -0,0 +1,13 @@
+package com.galgoya.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalgoyaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalgoyaUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galgoya/user/GalgoyaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galgoya.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalgoyaUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
